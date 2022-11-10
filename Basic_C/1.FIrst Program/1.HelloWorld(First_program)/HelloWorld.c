9963e9bdbcc7e600d5076a2d1b9146a7ac1cb0b6
Shivansh <shivanshkumar760@gmail.com>
2022-11-10 21:52:31 +0530

    initial commit

---
+++ Basic_C/1.FIrst Program/1.HelloWorld(First_program)/HelloWorld.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//#include <stdio.h> includes the standard input output library functions
+
+
+int main()//int main() The main() function is the entry point of every program in c language.
+{
+    //printf() The printf() function is used to print data on the console.
+    printf("Hello, world!");
+
+    //return 0 The return 0 statement, returns execution status to the OS. The 0 value is used for successful execution and 1 for unsuccessful execution.
+
+    return 0;
+}
---
+++ Basic_C/2.Variable Declaration/Variable_Declaration.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+    //Syntax of Variable declarations is:
+    /*
+        data_type variable_name;
+        
+        or 
+
+        data_type variable_name=<value>;
+
+
+    */
+
+
+   //integer type 
+   int a=1;
+   //char type
+   char b='b';
+   //float type
+   float d=1.0f; /*or*/ float e=1.1;
+   //double type
+   double f=1.222222222222334;
+
+
+   return 0;
+   
+}
